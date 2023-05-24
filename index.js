237891ecf6795063e6056e79fb134252e1a8d618
jonathan Quiroga <quirogaalan703@gmail.com>
2023-05-23 20:38:59 -0600

    apartados de css ordenados

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+/* letras con movimiento */
+
 const typed = new Typed('.typed', {
     strings: [
         '<i class="letras" >Soy Jonathan Quiroga </i>',
@@ -17,3 +19,44 @@ const typed = new Typed('.typed', {
 	contentType: 'html', // 'html' o 'null' para texto sin formato
 
 });
+
+
+/* ligar los enlaces del menu con la vista del apartado  */
+
+
+// Obtenemos todas las secciones y enlaces del menú
+const sections = document.querySelectorAll('section');
+const menuLinks = document.querySelectorAll('nav ul li a');
+
+// Función para comprobar qué sección está visible en la ventana
+function checkSectionInView() {
+  // Obtenemos la posición vertical del scroll y la altura de la ventana
+  const scrollPosition = window.pageYOffset;
+  const windowHeight = window.innerHeight;
+
+  // Recorremos las secciones para verificar cuál está en la ventana
+  sections.forEach(section => {
+    const sectionTop = section.offsetTop;
+    const sectionHeight = section.offsetHeight;
+    const sectionId = section.getAttribute('id');
+    
+    if (scrollPosition >= sectionTop - windowHeight / 2 &&
+        scrollPosition < sectionTop + sectionHeight - windowHeight / 2) {
+      // Removemos la clase "active" de todos los enlaces
+      menuLinks.forEach(link => link.classList.remove('active'));
+
+      // Agregamos la clase "active" al enlace correspondiente a la sección visible
+      document.querySelector(`nav ul li a[href="#${sectionId}"]`).classList.add('active');
+    }
+  });
+}
+
+// Agregamos un controlador de eventos al evento scroll
+window.addEventListener('scroll', checkSectionInView);
+
+
+
+
+
+
+
